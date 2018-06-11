cfe314bdb179d1f7bf9964a5d256c2e5a0b941b9
Cavin Dsouza <cavin46016@gmail.com>
2018-06-11 03:08:36 -0400

    Create demo_participant_shuffling.py

---
+++ demo_participant_shuffling.py
@@ -0,0 +1,27 @@
+import random
+
+participants = ['Damien', 'Maxime', 'Guy', 'Christine', 'Clément', 'Gaëlle', 'Denis', 
+ 'Katia', 'Maelys', 'Josian', 'Lucas', 'Isabelle', 'Pascal', 'Julien']
+
+random.shuffle(participants)
+
+for i in range(len(participants)):
+    print(participants[i],"buys for",participants[(i+1)%(len(participants))])
+    
+"""
+OUTPUT:
+Guy buys for Christine
+Christine buys for Josian
+Josian buys for Gaëlle
+Gaëlle buys for Katia
+Katia buys for Julien
+Julien buys for Pascal
+Pascal buys for Maxime
+Maxime buys for Clément
+Clément buys for Damien
+Damien buys for Lucas
+Lucas buys for Maelys
+Maelys buys for Denis
+Denis buys for Isabelle
+Isabelle buys for Guy
+"""
